79611269350174426f29ce7999aeeaed524b8140
Ryan Hennes <ryan.c.hennnes@gmail.com>
2020-05-22 19:29:32 -0700

    completed solution

--- [Easy] Yahtzee Upper Section Scoring.py
+++ [Easy] Yahtzee Upper Section Scoring.py
@@ -18,7 +18,38 @@ yahtzee_upper([1, 1, 1, 3, 3]) => 6
 yahtzee_upper([1, 2, 3, 4, 5]) => 5
 yahtzee_upper([6, 6, 6, 6, 6]) => 30
 
+Optional Bonus
+Efficiently handle inputs that are unsorted and much larger, both in the number of dice and in the number of sides per die. (For the purpose of this bonus challenge, you want the maximum value of some number k, times the number of times k appears in the input.)
+
+yahtzee_upper([1654, 1654, 50995, 30864, 1654, 50995, 22747,
+    1654, 1654, 1654, 1654, 1654, 30864, 4868, 1654, 4868, 1654,
+    30864, 4868, 30864]) => 123456
+There's no strict limit on how fast it needs to run. That depends on your language of choice. But for rough comparison, my Python solution on this challenge input, consisting of 100,000 values between 1 and 999,999,999 takes about 0.2 seconds (0.06 seconds not counting input parsing).
+
+If you're preparing for a coding interview, this is a good opportunity to practice runtime complexity. Try to find a solution that's linear (O(N)) in both time and space requirements.
+
 @author: ryanhennes
 """
 
+#All cases solution
+def yahtzee_upper(rolls):
+    #calculating total scores for each roll and storing in a list that can handle up to 100,000 scores
+    scores = [0]* (100000)
+    for i in range(0, len(rolls)):
+        scores[rolls[i]] = scores[rolls[i]] + 1
+    
+    #looping through the list and parsing for the largest score
+    maxRoll = scores[0]
+    for i in range(len(scores)):
+        if (scores[i] * i > maxRoll):
+            maxRoll = scores[i] * i
+    return maxRoll
 
+print(yahtzee_upper([2, 3, 5, 5, 6]))
+print(yahtzee_upper([1, 1, 1, 1, 3]))
+print(yahtzee_upper([1, 1, 1, 3, 3]))
+print(yahtzee_upper([1, 2, 3, 4, 5]))
+print(yahtzee_upper([6, 6, 6, 6, 6]))
+print(yahtzee_upper([1654, 1654, 50995, 30864, 1654, 50995, 22747,
+    1654, 1654, 1654, 1654, 1654, 30864, 4868, 1654, 4868, 1654,
+    30864, 4868, 30864]))
